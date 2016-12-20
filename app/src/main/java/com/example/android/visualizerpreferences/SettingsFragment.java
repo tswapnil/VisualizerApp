c5f43aeb36f830a7a2e6194e1badd57bc8d4352e
tswapnil <swtaneja@eng.ucsd.edu>
2016-12-19 17:38:16 -0800

    Fixed Bug

--- app/src/main/java/com/example/android/visualizerpreferences/SettingsFragment.java
+++ app/src/main/java/com/example/android/visualizerpreferences/SettingsFragment.java
@@ -8,12 +8,13 @@ import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
+import android.widget.Toast;
 
 /**
  * Created by Swapnil on 18-12-2016.
  */
 
-public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener{
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener, Preference.OnPreferenceChangeListener{
     /**
      * Called during {@link #onCreate(Bundle)} to supply the preferences for this fragment.
      * Subclasses are expected to call {@link #setPreferenceScreen(PreferenceScreen)} either
@@ -38,6 +39,9 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
             }
 
         }
+
+        Preference pref = findPreference(getString(R.string.pref_size_key));
+        pref.setOnPreferenceChangeListener(this);
     }
     private void setPreferenceSummary(Preference pref , String value){
        if(pref instanceof ListPreference){
@@ -73,5 +77,38 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
     public void onDestroy() {
         super.onDestroy();
         getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    /**
+     * Called when a Preference has been changed by the user. This is
+     * called before the state of the Preference is about to be updated and
+     * before the state is persisted.
+     *
+     * @param preference The changed Preference.
+     * @param newValue   The new value of the Preference.
+     * @return True to update the state of the Preference with the new value.
+     */
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+         Toast error = Toast.makeText(getContext(), "Please select a number between 0.1 and 3",Toast.LENGTH_SHORT);
+        String sizeKey = getString(R.string.pref_size_key);
+        if(preference.getKey().equals(sizeKey)){
+            String stringSize = ((String) (newValue)).trim();
+            if(stringSize.equals("")) {
+                stringSize = "1";
+            }
+            try {
+                float size = Float.parseFloat(stringSize);
+                if(size>3 || size <=0){
+                    error.show();
+                    return false;
+                }
+            }catch(NumberFormatException e){
+                error.show();
+                return false;
+            }
+        }
+        return true;
     }
 }
